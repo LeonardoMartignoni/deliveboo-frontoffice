7c8a836db901d0d345318b7501b531363ad1810c
LeonardoMartignoni <leonardo.martignonii@gmail.com>
2023-05-16 20:19:31 +0200

    Fixed bootstrap js

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -24,14 +24,20 @@ export default {
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse flex-grow-0 gap-3" id="navbarSupportedContent">
+        <div class="collapse navbar-collapse flex-grow-0" id="navbarSupportedContent">
           <!-- <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-            </ul> -->
-          <a href="#" class="btn btn-light rounded-pill px-4">Accedi</a>
-          <a href="#" class="btn btn-light rounded-pill px-4">Registrati</a>
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+          </ul> -->
+          <ul class="list-group d-flex flex-row gap-3">
+            <li>
+              <a href="#" class="btn btn-light rounded-pill px-4">Accedi</a>
+            </li>
+            <li>
+              <a href="#" class="btn btn-light rounded-pill px-4">Registrati</a>
+            </li>
+          </ul>
         </div>
       </div>
     </nav>
@@ -43,5 +49,11 @@ export default {
 
 .navbar-brand > img {
   height: $nav-height;
+}
+
+ul {
+  li {
+    list-style: none;
+  }
 }
 </style>
