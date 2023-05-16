c222741dbfec5235f1fb9740f30c9dfe5eb0ae47
LeonardoMartignoni <leonardo.martignonii@gmail.com>
2023-05-16 19:05:51 +0200

    Added restaurants list

--- src/components/OurRestaurants.vue
+++ src/components/OurRestaurants.vue
@@ -4,7 +4,18 @@
 export default {
   data() {
     return {
-      // title: "Hello world",
+      restaurantTypes: ["Italiano", "Americano", "Cinese", "Giapponese", "Messicano", "Indiano", "Pesce", "Carne", "Pizza", "Sushi", "Vegana"],
+      restaurantList: [
+        { thumbnail: "/images/restaurants_example/1.jpg", types: ["Italiano"], name: "Ristorante da Mario" },
+        { thumbnail: "/images/restaurants_example/2.jpg", types: ["Americano"], name: "Steakhouse Roadhouse" },
+        { thumbnail: "/images/restaurants_example/3.jpg", types: ["Cinese"], name: "Tempio del Dragone" },
+        { thumbnail: "/images/restaurants_example/4.jpg", types: ["Giapponese"], name: "Sushi Samurai" },
+        { thumbnail: "/images/restaurants_example/5.jpg", types: ["Messicano", "Carne"], name: "El Mariachi" },
+        { thumbnail: "/images/restaurants_example/6.jpg", types: ["Indiano", "Pesce", "Italiano"], name: "Taj Mahal" },
+        { thumbnail: "/images/restaurants_example/7.jpg", types: ["Pesce", "Italiano"], name: "Mare e Monti" },
+        { thumbnail: "/images/restaurants_example/8.jpg", types: ["Carne", "Italiano"], name: "La Brace" },
+        { thumbnail: "/images/restaurants_example/9.jpg", types: ["Pizza", "Italiano"], name: "Pizzeria Napoli" },
+      ],
     };
   },
 
@@ -19,10 +30,10 @@ export default {
     <div class="container">
       <h2 class="fs-1">I nostri ristoranti</h2>
 
-      <div class="row row-cols-4 mt-5">
+      <div class="row mt-5">
         <!-- Advanced search -->
-        <div class="col">
-          <div class="border border-1 p-3 d-flex flex-column">
+        <div class="col-3">
+          <div class="border border-1 p-3 d-flex flex-column h-100">
             <h5>Ricerca avanzata</h5>
 
             <div class="input-fields mt-1">
@@ -38,63 +49,34 @@ export default {
             <h5 class="mt-4">Ricerca avanzata</h5>
 
             <div class="restaurant-type-filter d-flex flex-column gap-1">
-              <div class="form-check">
-                <input class="form-check-input" type="checkbox" value="" id="Italiano" />
-                <label class="form-check-label" for="Italiano"> Italiano </label>
-              </div>
-              <div class="form-check">
-                <input class="form-check-input" type="checkbox" value="" id="Americano" />
-                <label class="form-check-label" for="Americano"> Americano </label>
-              </div>
-              <div class="form-check">
-                <input class="form-check-input" type="checkbox" value="" id="Cinese" />
-                <label class="form-check-label" for="Cinese"> Cinese </label>
-              </div>
-              <div class="form-check">
-                <input class="form-check-input" type="checkbox" value="" id="Giapponese" />
-                <label class="form-check-label" for="Giapponese"> Giapponese </label>
-              </div>
-              <div class="form-check">
-                <input class="form-check-input" type="checkbox" value="" id="Messicano" />
-                <label class="form-check-label" for="Messicano"> Messicano </label>
-              </div>
-              <div class="form-check">
-                <input class="form-check-input" type="checkbox" value="" id="Indiano" />
-                <label class="form-check-label" for="Indiano"> Indiano </label>
-              </div>
-              <div class="form-check">
-                <input class="form-check-input" type="checkbox" value="" id="Pesce" />
-                <label class="form-check-label" for="Pesce"> Pesce </label>
-              </div>
-              <div class="form-check">
-                <input class="form-check-input" type="checkbox" value="" id="Carne" />
-                <label class="form-check-label" for="Carne"> Carne </label>
-              </div>
-              <div class="form-check">
-                <input class="form-check-input" type="checkbox" value="" id="Pizza" />
-                <label class="form-check-label" for="Pizza"> Pizza </label>
-              </div>
-              <div class="form-check">
-                <input class="form-check-input" type="checkbox" value="" id="Sushi" />
-                <label class="form-check-label" for="Sushi"> Sushi </label>
-              </div>
-              <div class="form-check">
-                <input class="form-check-input" type="checkbox" value="" id="Vegana" />
-                <label class="form-check-label" for="Vegana"> Vegana </label>
+              <div class="form-check" v-for="typology in restaurantTypes">
+                <input class="form-check-input" type="checkbox" value="" :id="typology.toLowerCase()" />
+                <label class="form-check-label" :for="typology.toLowerCase()"> {{ typology }} </label>
               </div>
             </div>
           </div>
         </div>
 
         <!-- Restaurants List -->
-        <div class="col">
-          <div class="border border-1"></div>
-        </div>
-        <div class="col">
-          <div class="border border-1"></div>
-        </div>
-        <div class="col">
-          <div class="border border-1"></div>
+        <div class="col-9">
+          <div class="row row-cols-3 gy-4">
+            <div v-for="restaurant in restaurantList" class="col">
+              <div class="border border-1 restaurant-border">
+                <img class="restaurant-thumbnail img-fluid" :src="restaurant.thumbnail" />
+
+                <!-- Restaurant details -->
+                <div class="restaurant-details p-3">
+                  <h5 class="m-0">{{ restaurant.name }}</h5>
+                  <div class="mb-2 mt-1">
+                    <span class="restaurant-type">
+                      {{ restaurant.types.join(" - ") }}
+                    </span>
+                  </div>
+                  <a class="btn btn-primary rounded-pill text-white w-100" href="#">Menù</a>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -103,4 +85,24 @@ export default {
 
 <style lang="scss" scoped>
 @import "../styles/partials/variables";
+
+.restaurant-border {
+  border-radius: 5px;
+}
+
+.restaurant-thumbnail {
+  height: 170px;
+  width: 100%;
+  object-fit: cover;
+}
+
+.restaurant-type {
+  font-size: 0.9rem;
+}
+
+.restaurant-details {
+  h5 {
+    line-break: anywhere;
+  }
+}
 </style>
