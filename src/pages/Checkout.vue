c2f4f9911226ce485297db737bd5639045c44450
GioGio4 <g.ceccobelli@outlook.it>
2023-05-23 15:56:14 +0200

    Add Braintree Payment

--- src/pages/Checkout.vue
+++ src/pages/Checkout.vue
@@ -7,8 +7,24 @@ export default {
     return {
       store,
 
+
     };
   },
+
+  created() {
+    var button = document.querySelector('#submit-button');
+
+    braintree.dropin.create({
+      authorization: 'sandbox_g42y39zw_348pk9cgf3bgyw2b',
+      selector: '#dropin-container'
+    }, function (err, instance) {
+      button.addEventListener('click', function () {
+        instance.requestPaymentMethod(function (err, payload) {
+          // Submit payload.nonce to your server
+        });
+      })
+    });
+  }
 
 };
 </script>
@@ -77,6 +93,9 @@ export default {
         <!-- Box Pagamento -->
         <div class="col-12">
 
+          <div id="dropin-container"></div>
+          <button id="submit-button" class="button-payment button--small button--green">Purchase</button>
+
         </div>
 
       </div>
@@ -91,5 +110,39 @@ export default {
 img {
   width: 100px;
   height: 100px;
+}
+
+.button-payment {
+  cursor: pointer;
+  font-weight: 500;
+  left: 3px;
+  line-height: inherit;
+  position: relative;
+  text-decoration: none;
+  text-align: center;
+  border-style: solid;
+  border-width: 1px;
+  border-radius: 3px;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  display: inline-block;
+}
+
+.button--small {
+  padding: 10px 20px;
+  font-size: 0.875rem;
+}
+
+.button--green {
+  outline: none;
+  background-color: #64d18a;
+  border-color: #64d18a;
+  color: white;
+  transition: all 200ms ease;
+}
+
+.button--green:hover {
+  background-color: #8bdda8;
+  color: white;
 }
 </style>
