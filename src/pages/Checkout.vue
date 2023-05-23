f446b3813d3fb6a4b08919aa48873120a9515389
LeonardoMartignoni <leonardo.martignonii@gmail.com>
2023-05-23 23:45:18 +0200

    Refactor commit

--- src/pages/Checkout.vue
+++ src/pages/Checkout.vue
@@ -52,20 +52,22 @@ export default {
     <div class="container">
       <h2 class="fs-1">Checkout</h2>
 
-      <div class="row mt-5">
+      <div class="row mt-5 gx-4">
         <!-- Box prodotti nel carrello -->
-        <div class="col-6 border p-3">
-          <h3>Il tuo Ordine</h3>
+        <div class="col-6">
+          <div class="form-content border p-3 h-100">
+            <h3>Il tuo Ordine</h3>
 
-          <div v-for="item in store.cartItems" class="card mb-3">
-            <div class="row">
-              <div class="col-md-4">
-                <img :src="item.itemImage" class="rounded-start" alt="image" />
-              </div>
-              <div class="col-md-8">
-                <div class="card-body">
-                  <h5 class="card-title">{{ item.itemName }}</h5>
-                  <p class="card-text">Pezzi: {{ item.itemQuantity }}</p>
+            <div v-for="item in store.cartItems" class="card mb-3">
+              <div class="row">
+                <div class="col-md-4">
+                  <img :src="item.itemImage" class="rounded-start" alt="image" />
+                </div>
+                <div class="col-md-8">
+                  <div class="card-body">
+                    <h5 class="card-title">{{ item.itemName }}</h5>
+                    <p class="card-text">Pezzi: {{ item.itemQuantity }}</p>
+                  </div>
                 </div>
               </div>
             </div>
@@ -73,36 +75,38 @@ export default {
         </div>
 
         <!-- Box form dati utente -->
-        <div class="col-6 border p-3">
-          <h3>Inserisci i dati per la consegna</h3>
-          <form method="POST" @submit.prevent="submitForm">
-            <div class="mb-3">
-              <label for="customer_name" class="form-label">Nome</label>
-              <input type="text" class="form-control" id="customer_name" v-model="formData.customer_name" />
-            </div>
-            <div class="mb-3">
-              <label for="customer_surname" class="form-label">Cognome</label>
-              <input type="text" class="form-control" id="customer_surname" v-model="formData.customer_surname" />
-            </div>
-            <div class="mb-3">
-              <label for="customer_address" class="form-label">Indirizzo</label>
-              <input type="text" class="form-control" id="customer_address" v-model="formData.customer_address" />
-            </div>
-            <div class="mb-3">
-              <label for="customer_mail" class="form-label">Email</label>
-              <input type="email" class="form-control" id="customer_mail" v-model="formData.customer_mail" />
-            </div>
-            <div class="mb-3">
-              <label for="customer_phone_number" class="form-label">Telefono</label>
-              <input type="text" class="form-control" id="customer_phone_number" v-model="formData.customer_phone_number" maxlength="11" />
-            </div>
-            <div class="mb-3">
-              <label for="note" class="form-label">Note</label>
-              <textarea type="text" class="form-control" id="note" rows="3" v-model="formData.note"></textarea>
-            </div>
+        <div class="col-6">
+          <div class="form-content border p-3">
+            <h3>Inserisci i dati per la consegna</h3>
+            <form method="POST" @submit.prevent="submitForm">
+              <div class="mb-3">
+                <label for="customer_name" class="form-label">Nome</label>
+                <input type="text" class="form-control" id="customer_name" v-model="formData.customer_name" />
+              </div>
+              <div class="mb-3">
+                <label for="customer_surname" class="form-label">Cognome</label>
+                <input type="text" class="form-control" id="customer_surname" v-model="formData.customer_surname" />
+              </div>
+              <div class="mb-3">
+                <label for="customer_address" class="form-label">Indirizzo</label>
+                <input type="text" class="form-control" id="customer_address" v-model="formData.customer_address" />
+              </div>
+              <div class="mb-3">
+                <label for="customer_mail" class="form-label">Email</label>
+                <input type="email" class="form-control" id="customer_mail" v-model="formData.customer_mail" />
+              </div>
+              <div class="mb-3">
+                <label for="customer_phone_number" class="form-label">Telefono</label>
+                <input type="text" class="form-control" id="customer_phone_number" v-model="formData.customer_phone_number" maxlength="11" />
+              </div>
+              <div class="mb-3">
+                <label for="note" class="form-label">Note</label>
+                <textarea type="text" class="form-control" id="note" rows="3" v-model="formData.note"></textarea>
+              </div>
 
-            <button type="submit" class="btn btn-primary w-100 rounded-pill text-white">Invia l'ordine</button>
-          </form>
+              <button type="submit" class="btn btn-primary w-100 rounded-pill text-white">Invia l'ordine</button>
+            </form>
+          </div>
         </div>
 
         <!-- Box Pagamento -->
@@ -134,8 +138,6 @@ img {
   border-style: solid;
   border-width: 1px;
   border-radius: 3px;
-  -webkit-appearance: none;
-  -moz-appearance: none;
   display: inline-block;
 }
 
